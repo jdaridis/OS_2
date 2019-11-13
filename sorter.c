5980eda1af38450b20f2c214d5b4c5e3b06d89d5
John Daridis <daridisioannis@gmail.com>
2019-11-13 21:31:51 +0200

    Corrected start and end offsets for file in sorter

--- sorter.c
+++ sorter.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
+#include <signal.h>
+#include <wait.h> 
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 #include "Record.h"
 #include "comparators.h"
 #ifdef __HEAP_SORT__ 
@@ -19,8 +25,12 @@ int main(int argc, char const *argv[]){
     int record_count = 0;
     int column = 0;
     int start;
+    int end;
     float frac;
-    int size;
+    int size = -1;
+    int fd = -1;
+    int bytes_read;
+    struct stat stat;
     while(--argc){
         if(strcmp(*argv, "-f") == 0){
             file = fopen(*(argv + 1), "r");
@@ -29,36 +39,29 @@ int main(int argc, char const *argv[]){
             column = atoi(*(argv + 1)) - 1;
         } else if(strcmp(*argv, "-s") == 0){
             start = atoi(*(argv + 1));
-            frac = atof(*(argv + 2));
+            end = atoi(*(argv + 2));
         } else if(strcmp(*argv, "-size") == 0){
             size = atoi(*(argv + 1));
         }
         argv++;
     }
+
+    printf("File descreiptor %d, column %d, start %d, end %d\n", fd, column, start, end);
+
+    record_count = end - start;
     
-    record_count = size*frac;
-   /*  while(!feof(file) && (float)record_count < size*frac){
-        
-        fread(&temp_rec, sizeof(Record), 1, file);
-        if(!feof(file)){
-            record_count++;
-        }
-        
-    } */
-
-    fseek(file, start*sizeof(Record), SEEK_SET);
-
-    printf("File size %d\n", record_count);
-
     records = malloc(record_count*sizeof(Record*));
     int i = 0;
     for(i=0;i<record_count;i++){
         records[i] = malloc(sizeof(Record));
     }
+    printf("File size %d\n", record_count);
 
+    printf("here\n");
+    fseek(file, start*sizeof(Record), SEEK_SET);
 
     i = 0;
-     while(!feof(file) && (float)i < record_count){
+    while(!feof(file) && i < record_count){
         if(!feof(file)){
             fread(records[i], sizeof(Record), 1, file);
             i++;
@@ -74,9 +77,6 @@ int main(int argc, char const *argv[]){
     printf("Using quicksort\n");
     #endif
 
-    
-
-
     printf("----------------------------\n");
 
     for(i=0;i<record_count;i++){
@@ -85,6 +85,8 @@ int main(int argc, char const *argv[]){
 		records[i]->home_address, records[i]->home_number, records[i]->city, records[i]->mail_sector, \
 		records[i]->salary);
     }
+
+    kill(getppid(), SIGUSR2);
 
     return 0;
 
